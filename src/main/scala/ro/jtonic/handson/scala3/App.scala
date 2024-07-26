c8e0942ad27d6457d7f3085a8b38eb9fa8e2946d
tony <antonel-ernest.pazargic@ing.com>
2024-07-26 20:27:15 +0300

    To test metals nvim dap

--- src/main/scala/ro/jtonic/handson/scala3/App.scala
+++ src/main/scala/ro/jtonic/handson/scala3/App.scala
@@ -5,8 +5,8 @@ import scala.io.StdIn.readLine
 object App:
 
   def main(args: Array[String]): Unit =
-    println("How are you?")
     println("What's your name?")
+    println("How are you?")
     readLine() match
       case null => println("No name provided!")
       case name => println(s"Hello, $name!")
